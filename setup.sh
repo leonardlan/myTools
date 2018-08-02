0319107299d11e5192d2d7c95d604552c71ce496
Leonard Lan <leonard@leonard-P770ZM-G>
2018-08-01 22:27:55 -0700

    merge

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # Creates symlinks. Be careful, this will delete existing.
 
-ln -sfn `realpath useful_commands.txt` ~/useful_commands.txt
-ln -sfn `realpath install_my_stuff.txt` ~/install_my_stuff.txt
-ln -sfn `realpath bash_aliases` ~/.bash_aliases
-ln -sfn `realpath Preferences.sublime-settings` ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-ln -sfn `realpath pythonrc` ~/.pythonrc
+ln -sfn `readlink -f useful_commands.txt` ~/useful_commands.txt
+ln -sfn `readlink -f install_my_stuff.txt` ~/install_my_stuff.txt
+ln -sfn `readlink -f bash_aliases` ~/.bash_aliases
+ln -sfn `readlink -f Preferences.sublime-settings` ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
