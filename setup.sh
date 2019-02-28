acf27b0546336f9a69ec50e08414e972ba77bcf7
Leonard Lan <llan@airbud.com>
2019-02-28 12:20:51 -0800

    Add cron job in setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,15 @@ ln -sfn `readlink -f bash_aliases` ~/.bash_aliases
 ln -sfn `readlink -f Preferences.sublime-settings` ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 ln -sfn `readlink -f pythonrc` ~/.pythonrc
 
+add_line_to_file() {
+    grep -qF -- "$1" "$2" || echo "$1" >> "$2"
+}
+
 # Add source line to .bashrc file if not existing
-LINE='source ~/.my_bashrc'
-FILE=~/.bashrc
-grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+add_line_to_file 'source ~/.my_bashrc' ~/.bashrc
+
+# Add cron job
+crontab -l > /tmp/mycron
+add_line_to_file "0 9-18 * * 1-5 /home/llan/myTools/notify-time.sh > /dev/null 2>&1 # Notify me every so often" /tmp/mycron
+crontab /tmp/mycron
+rm /tmp/mycron
