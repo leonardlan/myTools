afecb314c9811bfb7f875d030f55a422b03b898f
Leonard Lan <leonard.lan@balenwsl027.bron.local>
2019-03-27 14:48:24 -0700

    Fix cube_of_spheres

--- maya_scripts/cube_of_spheres.py
+++ maya_scripts/cube_of_spheres.py
@@ -1,29 +1,32 @@
-from mayatools.context import selection
+''' Creates a cube of spheres with input size '''
 
 
-with selection():
-    SIZE = 5
-    def foo(input):
-        return -1 * (SIZE - 1) + input * 2
+SIZE = 5
 
-    result = cmds.promptDialog(
-        title='Cube Of Spheres',
-        message='Enter Size:',
-        button=['Create', 'Cancel'],
-        defaultButton='Create',
-        cancelButton='Cancel',
-        dismissString='Cancel',
-        style="integer",
-        text=SIZE
-    )
+def get_movement(input):
+    return -1 * (SIZE - 1) + input * 2
 
-    if result == 'Create':
-        SIZE = int(cmds.promptDialog(query=True, text=True))
-        boundaries = [0, SIZE - 1]
-        for x in range(SIZE):
-            for y in range(SIZE):
-                for z in range(SIZE):
-                    if x not in boundaries and y not in boundaries and z not in boundaries:
-                        continue
-                    cmds.polySphere()
-                    cmds.move(foo(x), foo(y), foo(z))
+result = cmds.promptDialog(
+    title='Cube Of Spheres',
+    message='Enter Size:',
+    button=['Create', 'Cancel'],
+    defaultButton='Create',
+    cancelButton='Cancel',
+    dismissString='Cancel',
+    style='integer',
+    text=SIZE
+)
+
+if result == 'Create':
+    SIZE = int(cmds.promptDialog(query=True, text=True))
+    boundaries = [0, SIZE - 1]
+    for x in range(SIZE):
+        move_x = get_movement(x)
+        for y in range(SIZE):
+            move_y = get_movement(y)
+            for z in range(SIZE):
+                if x not in boundaries and y not in boundaries and z not in boundaries:
+                    continue
+                cmds.polySphere()
+                move_z = get_movement(z)
+                cmds.move(move_x, move_y, move_z)
