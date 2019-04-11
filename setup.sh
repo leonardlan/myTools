53b3ba92d30d173087b109847de1d1f3ede2647c
Leonard Lan <leonard.lan@balenwsl027.bron.local>
2019-04-11 16:31:30 -0700

    Adding sublime snippert python_function

--- setup.sh
+++ setup.sh
@@ -7,10 +7,13 @@ ln -sfn `readlink -f bash_aliases` ~/.bash_aliases
 ln -sfn `readlink -f gitconfig` ~/.gitconfig
 ln -sfn `readlink -f gitignore_global` ~/.gitignore_global
 ln -sfn `readlink -f my_bashrc` ~/.my_bashrc
-ln -sfn `readlink -f Preferences.sublime-settings` ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-ln -sfn "`readlink -f 'Default (Linux).sublime-keymap'`" ~/.config/sublime-text-3/Packages/User/'Default (Linux).sublime-keymap'
 ln -sfn `readlink -f pythonrc` ~/.pythonrc
 ln -sfn `readlink -f useful_commands.txt` ~/useful_commands.txt
+
+# Setup my sublime prefs
+ln -sfn `readlink -f Preferences.sublime-settings` ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -sfn "`readlink -f 'Default (Linux).sublime-keymap'`" ~/.config/sublime-text-3/Packages/User/'Default (Linux).sublime-keymap'
+ln -sfn `readlink -f sublime/snippets/python_function.sublime-snippet` ~/.config/sublime-text-3/Packages/User/python_function.sublime-snippet
 
 add_line_to_file() {
     grep -qF -- "$1" "$2" || echo "$1" >> "$2"
