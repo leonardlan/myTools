93e4885ca5e1b931c4bc28096d9ef6ef259bca45
Leonard Lan <real.leonard.lan@gmail.com>
2020-02-27 17:48:23 -0800

    Adding my Qt App with system tray icon shortcuts

---
+++ python/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+from PyQt4.QtGui import QSystemTrayIcon, QApplication
+from PyQt4.QtCore import QFile, QString
+
+import utils
+
+
+class App:
+
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self._load_stylesheet()
+        self._create_tray_icon()
+
+    def _create_tray_icon(self):
+        self.tray = utils.TrayIcon(self.reload)
+
+    def _load_stylesheet(self):
+        fil = QFile(os.path.join(utils.MYTOOLS, 'python/stylesheet.qss'))
+        fil.open(QFile.ReadOnly)
+        self.app.setStyleSheet(QString(fil.readAll()))
+        fil.close()
+
+    def run(self):
+        # Enter Qt application main loop
+        self.app.exec_()
+        sys.exit()
+
+    def reload(self):
+        '''Reloads utils and redraws menu'''
+        print 'Reloading app'
+        try:
+            reload(utils)
+        except Exception, e:
+            self.tray.showMessage(
+                'Error',
+                'Unable to reload utils module!\n%s' % e,
+                QSystemTrayIcon.Critical
+            )
+        else:
+            self._create_tray_icon()
+        self._load_stylesheet()
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
---
+++ python/utils.py
@@ -0,0 +1,137 @@
+import os
+import subprocess
+import sys
+
+from functools import partial
+from ruamel.yaml import YAML
+
+from PyQt4.QtGui import QMenu, QIcon, QSystemTrayIcon, QCursor
+
+
+HOME = os.path.expanduser('~')
+MYTOOLS = os.path.expanduser('~/myTools')
+SANDBOX = os.path.expanduser('~/dev/sandbox')
+ACTIONS_CONFIG_FILE = os.path.join(MYTOOLS, 'config/actions.yaml')
+
+
+def run(cmd, keep_open=False, title=''):
+    '''Run a command in new subprocess terminal.
+
+    Args:
+        cmd (str): Command to run.
+        keep_open (bool): If True, keeps terminal open after command run.
+        title (str): Title of terminal.
+    '''
+    if keep_open:
+        cmd = "mate-terminal -e 'bash -i -c \"%s; bash\"'" % cmd
+    if title:
+        cmd += " -t '%s'" % title
+    print 'Running:', cmd
+    subprocess.Popen(cmd, shell=True)
+
+
+def open_sublime(path, chrome=False, n=False, a=False, toggle_side_bar=False):
+    '''Open file in sublime.'''
+    print 'Opening', path
+    path = os.path.join(HOME, path)
+    cmd = [os.path.expanduser('~/sublime_text_3/sublime_text'), path]
+    if n:
+        cmd.insert(1, '-n')
+    if a:
+        cmd.insert(2, '-a')
+    if toggle_side_bar:
+        cmd[1:1] = ['--command', 'toggle_side_bar']
+    subprocess.call(cmd)
+    if chrome:
+        subprocess.call(['google-chrome', path])
+
+
+def open_terminal(dir_, title=''):
+    '''Open Terminal at directory.'''
+    run('mate-terminal --working-directory=' + os.path.expanduser(dir_), title=title)
+
+
+def icon(name):
+    '''Icon from name or file path to icon.'''
+    if os.path.exists(name):
+        return QIcon(name)
+    return QIcon(os.path.join(MYTOOLS, 'img', name + '.png'))
+
+
+class TrayIcon(QSystemTrayIcon):
+    '''My implementation of QSystemTrayIcon.'''
+
+    def __init__(self, reload_func):
+        super(TrayIcon, self).__init__(icon('ll'))
+        self.reload_func = reload_func
+        self.setContextMenu(self.make_menu())
+        self.setToolTip("Leonard's Quick Links\nMiddle-mouse click to reload")
+        self.activated.connect(self._activated)
+        self.show()
+
+    def get_actions(self):
+        '''Load actions from config file.'''
+        yaml = YAML()
+        with open(ACTIONS_CONFIG_FILE, 'r') as fil:
+            return yaml.load(fil)
+
+    def _setup_actions(self, menu, actions):
+        '''Setup actions.'''
+        for text, data in actions.items():
+            # Separator.
+            if data == 'separator':
+                menu.addSeparator()
+                continue
+
+            # Menu with sub actions.
+            if data.get('actions'):
+                new_menu = menu.addMenu(text)
+
+                # Icon.
+                if data.get('icon'):
+                    new_menu.setIcon(icon(data.get('icon')))
+
+                # Recursively setup sub actions in current menu.
+                self._setup_actions(new_menu, data.get('actions'))
+                continue
+
+            # Action.
+            action = menu.addAction(text)
+            if data.get('cmd'):
+                action.triggered.connect(partial(run, data.get('cmd'), **data.get('kwargs', {})))
+            elif data.get('path'):
+                action.triggered.connect(partial(open_sublime, data.get('path')))
+            elif data.get('terminal_path'):
+                action.triggered.connect(
+                    partial(open_terminal, data.get('terminal_path'), **data.get('kwargs', {})))
+
+            # Set icon.
+            if data.get('icon'):
+                action.setIcon(icon(data.get('icon')))
+
+    def make_menu(self):
+        '''Make and return menu.'''
+        # Setup menu.
+        menu = QMenu()
+        menu.setTearOffEnabled(True)
+
+        # Setup actions from config file.
+        self._setup_actions(menu, self.get_actions())
+
+        # Reload Me action.
+        action = menu.addAction(icon('reload'), '&Reload Me')
+        action.triggered.connect(self.reload_func)
+        menu.addSeparator()
+
+        # Exit action.
+        action = menu.addAction(icon('exit'), '&Exit')
+        action.triggered.connect(sys.exit)
+
+        return menu
+
+    def _activated(self, reason):
+        '''Show context menu on left-click.'''
+        if reason == QSystemTrayIcon.Trigger:
+            self.contextMenu().popup(QCursor().pos())
+        elif reason == QSystemTrayIcon.MiddleClick:
+            self.reload_func()
