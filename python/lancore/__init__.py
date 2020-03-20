4ec9bcec24d08cb6d13b55c284d81b9439bf2d03
Leonard Lan <real.leonard.lan@gmail.com>
2020-03-19 23:10:10 -0700

    Remove unusued numpy Add microseconds and milliseconds to human_time()

--- python/lancore/__init__.py
+++ python/lancore/__init__.py
@@ -2,7 +2,6 @@
 import datetime
 import json
 import os
-import numpy
 import sys
 import tempfile
 import time
@@ -61,26 +60,39 @@ INTERVALS = OrderedDict([
     ('minute', 60),
     ('second', 1)
 ])
-
+LESS_THAN_A_SECOND = [
+    'millisecond',
+    'microsecond',
+]
 
 def human_time(seconds, decimals=1):
-    '''Human-readable time from seconds (ie. 5 days and 2 hours).
+    '''Human-readable time from seconds (ie. 3600 -> '1 hour').
 
     Examples:
         >>> human_time(15)
         '15 seconds'
+        >>> human_time(60)
+        '1 minute'
         >>> human_time(3600)
         '1 hour'
         >>> human_time(3720)
         '1 hour and 2 minutes'
         >>> human_time(266400)
         '3 days and 2 hours'
+        >>> human_time(12345678890)
+        '39 years and 1 month'
+
         >>> human_time(-1.5)
         '-1.5 seconds'
         >>> human_time(0)
         '0 seconds'
+
+        Less than a second.
         >>> human_time(0.1)
         '100 milliseconds'
+        >>> human_time(1.1e-6)
+        '1.1 microseconds'
+
         >>> human_time(1)
         '1 second'
         >>> human_time(1.234, 2)
@@ -88,22 +100,36 @@ def human_time(seconds, decimals=1):
 
     Args:
         seconds (int or float): Duration in seconds.
-        decimals (int): Number of decimals.
+        decimals (int): Number of decimals. Only applies 0 < seconds < 60.
 
     Returns:
         str: Human-readable time.
     '''
-    input_is_int = isinstance(seconds, int)
+    is_int = isinstance(seconds, int)
     if seconds < 0:
-        return str(seconds if input_is_int else round(seconds, decimals)) + ' seconds'
+        # Negative time.
+        return str(seconds if is_int else round(seconds, decimals)) + ' seconds'
     elif seconds == 0:
+        # Zero time.
         return '0 seconds'
     elif 0 < seconds < 1:
-        # Return in milliseconds.
-        ms = int(seconds * 1000)
-        return '%i millisecond%s' % (ms, 's' if ms != 1 else '')
+        # Less than a second.
+        for interval in LESS_THAN_A_SECOND:
+            seconds *= 1000
+            quotient, remainder = divmod(seconds, 1)
+            if quotient > 0:
+                if remainder == 0:
+                    res = int(quotient)
+                else:
+                    res = round(seconds, decimals)
+                print quotient, remainder
+                print res
+                return '%s %s%s' % (str(res), interval, 's' if res != 1 else '')
+        # Less than a microsecond.
+        return '0 seconds'
     elif 1 < seconds < INTERVALS['minute']:
-        return str(seconds if input_is_int else round(seconds, decimals)) + ' seconds'
+        # A second to a minute.
+        return str(seconds if is_int else round(seconds, decimals)) + ' seconds'
 
     res = []
     for interval, count in INTERVALS.iteritems():
@@ -135,6 +161,20 @@ def time_me(func, *args, **kwargs):
     Running sleep(5)
     1/1 [2019-11-26 09:45:33.276782]: 5.0 seconds
 
+    Run multiple times
+    >>> time_me(time.sleep, 1, n=5)
+    Running sleep(1)
+    1/5 [2020-03-11 00:03:48.178749]: 1.0 seconds
+    2/5 [2020-03-11 00:03:49.179930]: 1.0 seconds
+    3/5 [2020-03-11 00:03:50.181081]: 1.0 seconds
+    4/5 [2020-03-11 00:03:51.182234]: 1.0 seconds
+    5/5 [2020-03-11 00:03:52.182661]: 1.0 seconds
+    Total time: 5.0 seconds
+    Average time: 1.0 seconds
+    Fastest time: 1.0 seconds
+    Slowest time: 1.0 seconds
+    Standard deviation: 0.00
+
     Args:
         func (function): Function to call.
         args (list): Args to pass to function.
@@ -148,36 +188,50 @@ def time_me(func, *args, **kwargs):
         ERROR('Invalid function: %s' % func)
         return
     n = kwargs.pop('n', 1)
+
+    # Print nice function call.
     args_str = [str(arg) for arg in args]
     params = ', '.join(args_str + ['%s=%s' % (key, str(val)) for key, val in kwargs.iteritems()])
     print 'Running %s(%s)' % (func.__name__, params)
+
+    # Run function n times.
     run_times = []
     res = None
     for count in range(n):
         sys.stdout.write('%i/%i [%s]: ' % (count + 1, n, str(datetime.datetime.now())))
         sys.stdout.flush()
         start = time.time()
+
+        # Execute function call.
         try:
             res = func(*args, **kwargs)
         except Exception, e:
+            # Function call errored.
             end = time.time()
             duration = end - start
             WARNING('\nFunction errored after %s: %s' % (human_time(duration), e))
             print traceback.format_exc().strip()
             return res
+
+        # Print how long did function call took
         end = time.time()
         duration = end - start
         run_times.append(duration)
         print human_time(duration)
 
     if n >= 2:
+        # Print total time and average time.
         total = sum(run_times)
         print 'Total time: %s' % human_time(total)
         print 'Average time: %s' % human_time(total / n)
+
     if n >= 3:
+        # Print simple stats.
+        import numpy
         print 'Fastest time: %s' % human_time(min(run_times))
         print 'Slowest time: %s' % human_time(max(run_times))
         print 'Standard deviation: %.2f' % numpy.std(run_times)
+
     return res
 
 
