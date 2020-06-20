5b2c1e585d798331df9d944778be7a08092bf8c3
Leonard Lan <real.leonard.lan@gmail.com>
2020-06-19 19:33:55 -0700

    Add dogs mock data

--- python/mock_data.py
+++ python/mock_data.py
@@ -5,6 +5,10 @@ from json_tools import load_json
 from lancore import MYTOOLS
 
 
+MOCK_DATA = os.path.join(MYTOOLS, 'mock_data')
+
 # Example data to fiddle around with.
-PEOPLE = load_json(os.path.join(MYTOOLS, 'mock_data', 'people.json'))
-PEOPLE_DICT = {people['id']: people for people in PEOPLE}
+DOGS = load_json(os.path.join(MOCK_DATA, 'dogs.json'))
+DOGS_DICT = {dog['id']: dog for dog in DOGS}
+PEOPLE = load_json(os.path.join(MOCK_DATA, 'people.json'))
+PEOPLE_DICT = {person['id']: person for person in PEOPLE}
