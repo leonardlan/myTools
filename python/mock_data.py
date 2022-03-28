63f321feb9db4de4e4eb50f7e4fbc75210d606dc
Leonard Lan <real.leonard.lan@gmail.com>
2022-03-28 12:29:06 -0700

    Add class and TYPE_TO_VAL to mock data

--- python/mock_data.py
+++ python/mock_data.py
@@ -12,3 +12,27 @@ DOGS = load_json(os.path.join(MOCK_DATA, 'dogs.json'))
 DOGS_DICT = {dog['id']: dog for dog in DOGS}
 PEOPLE = load_json(os.path.join(MOCK_DATA, 'people.json'))
 PEOPLE_DICT = {person['id']: person for person in PEOPLE}
+
+
+class SomeObject(object):
+    pass
+
+    @property
+    def some_property(self):
+        return 'Some property'
+
+
+TYPE_TO_VAL = {
+    'string': 'Hello!',
+    'int': 1234,
+    'float': 3.14,
+    'list': ['Vancouver', 'Calgary', 'Montreal'],
+    'tuple': (0, 1, 1, 2, 3, 5, 8, 13, 21, 34),
+    'set': set([1, 2, 3, 4]),
+    'dict': {
+        'British Columbia': {'capital': 'Victoria'},
+        'Ontario': {'capital': 'Toronto'}
+    },
+    'some_object': SomeObject(),
+    'class': SomeObject
+}
