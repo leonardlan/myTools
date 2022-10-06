590e14574c4f0bedf7918451b8f129b990917f54
Leonard Lan <real.leonard.lan@gmail.com>
2022-10-05 23:23:56 -0700

    - Print report for os_tools.list_files() - Add os_tools.remove_pyc_files()

--- python/os_tools.py
+++ python/os_tools.py
@@ -6,7 +6,7 @@ import python_compat
 
 
 def list_files(path='.', ext=None, print_found=False):
-    '''List files in folder recursively using os.walk().
+    '''List files in folder recursively using os.walk() with extensions filter.
 
     Args:
         path (str): Root directory to search. Defaults to current directory.
@@ -16,7 +16,7 @@ def list_files(path='.', ext=None, print_found=False):
     Returns:
         list: File paths.
     '''
-    full_paths = []
+    paths = []
 
     # Convert ext list to tuple.
     ext = tuple(ext) if isinstance(ext, list) else ext
@@ -31,7 +31,26 @@ def list_files(path='.', ext=None, print_found=False):
         for file in files:
             if ext is None or (ext and file.lower().endswith(ext)):
                 full_path = os.path.join(root, file)
-                full_paths.append(full_path)
+                paths.append(full_path)
                 if print_found:
                     print(full_path)
-    return full_paths
+
+    # Print report.
+    if print_found:
+        if len(paths) > 1:
+            print('\nFound {} paths'.format(len(paths)))
+        elif not paths:
+            print('Did not find any paths')
+
+    return paths
+
+
+def remove_pyc_files(path='.', print_found=True, dry_run=True):
+    '''Remove .pyc files from directory recursively.'''
+    paths = list_files(ext='.pyc', path=path, print_found=print_found)
+    for path in paths:
+        if dry_run:
+            print('Not removing in dry run: {}'.format(path))
+        else:
+            print('Removing {}'.format(path))
+            os.remove(path)
