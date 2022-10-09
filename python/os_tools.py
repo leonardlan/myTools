767a762a7dc3127020327261429ff96447b004d5
Leonard Lan <real.leonard.lan@gmail.com>
2022-10-08 22:54:46 -0700

    - add os_tools.walk_dir() generator - add os_tools.tree() - add os_tools.print_dir_sizes()

--- python/os_tools.py
+++ python/os_tools.py
@@ -1,23 +1,61 @@
 '''Tools extending os module.'''
 
 import os
-
 import python_compat
 
+from os.path import getsize, join
 
-def list_files(path='.', ext=None, print_found=False):
+
+def walk_dir(path='.', max_depth=None, ignore_dir=None, ignore_case=True):
+    '''Same as os.walk() but with max depth and ignore director(y/ies).
+
+    Kwargs:
+        path (str): Root directory to walk.
+        max_depth (int or None): Max depth to walk into. None means no max depth.
+        ignore_dir (str, [str], or None): Directories to ignore, match by name.
+        ignore_case (bool): If True, will match case-insensitive when ignore_dir is specified.
+
+    Returns:
+        generator: Same as os.walk()
+    '''
+    # Get ignore_dirs as set.
+    ignore_dir = ignore_dir or []
+    ignore_dirs = [ignore_dir] if not isinstance(ignore_dir, list) else ignore_dir
+    ignore_dirs = set(ignore_dirs) if not isinstance(ignore_dirs, set) else ignore_dirs
+    if ignore_case:
+        ignore_dirs = set(dir_.lower() for dir_ in ignore_dirs)
+
+    path = path.rstrip(os.sep)
+    num_sep = path.count(os.sep)
+    for root, dirs, files in os.walk(path):
+        yield root, dirs, files
+
+        # Remove sub directories if max depth reached.
+        num_sep_this = root.count(os.sep)
+        if isinstance(max_depth, int) and num_sep + max_depth <= num_sep_this:
+            del dirs[:]
+
+        # Ignore specified directories by running dirs.remove() since it's mutable.
+        if ignore_dirs:
+            for dir_ in dirs:
+                new_dir = dir_.lower() if ignore_case else dir_
+                if new_dir in ignore_dirs:
+                    dirs.remove(dir_)
+
+
+def list_files(path='.', ext=None, print_found=False, max_depth=None, ignore_dir=None):
     '''List files in folder recursively using os.walk() with extensions filter.
 
     Args:
         path (str): Root directory to search. Defaults to current directory.
         ext (str, [str], (str,)): Case-insensitive match file path using endswith().
         print_found (bool): Prints found paths while looping, if True.
+        max_depth (int or None): Max depth to walk into. None means no max depth.
+        ignore_dir (str, [str], or None): Directories to ignore, match by name.
 
     Returns:
         list: File paths.
     '''
-    paths = []
-
     # Convert ext list to tuple.
     ext = tuple(ext) if isinstance(ext, list) else ext
 
@@ -27,10 +65,12 @@ def list_files(path='.', ext=None, print_found=False):
     elif isinstance(ext, tuple):
         ext = tuple(e.lower() for e in ext)
 
-    for root, _, files in os.walk(path):
+    # Walk directory.
+    paths = []
+    for root, _, files in walk_dir(path=path, max_depth=max_depth, ignore_dir=ignore_dir):
         for file in files:
             if ext is None or (ext and file.lower().endswith(ext)):
-                full_path = os.path.join(root, file)
+                full_path = join(root, file)
                 paths.append(full_path)
                 if print_found:
                     print(full_path)
@@ -50,7 +90,36 @@ def remove_pyc_files(path='.', print_found=True, dry_run=True):
     paths = list_files(ext='.pyc', path=path, print_found=print_found)
     for path in paths:
         if dry_run:
-            print('Not removing in dry run: {}'.format(path))
+            print('Not removing since dry_run is True: {}'.format(path))
         else:
             print('Removing {}'.format(path))
             os.remove(path)
+    print('Removed {} .pyc files'.format(len(paths)))
+
+
+def tree(path='.', max_depth=None, ignore_dir=None, ignore_case=True):
+    '''Print folder tree from path.
+
+    Kwargs:
+        See walk_dir().
+    '''
+    gen = walk_dir(path=path, max_depth=max_depth, ignore_dir=ignore_dir, ignore_case=ignore_case)
+    for root, _, files in gen:
+        depth = root.replace(path, '').count(os.sep)
+        indent = ' ' * 4 * (depth)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (depth + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+
+def print_dir_sizes(path='.', max_depth=None, ignore_dir=None, ignore_case=True):
+    gen = walk_dir(path=path, max_depth=max_depth, ignore_dir=ignore_dir, ignore_case=ignore_case)
+    for root, _, files in gen:
+        size = 0
+        for name in files:
+            try:
+                size += getsize(join(root, name))
+            except Exception as err:
+                print('Unable to get dir size: {}'.format(err))
+        print('{} [{} bytes | {} files]'.format(root, size, len(files)))
