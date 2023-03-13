fe4bff8c59d3143450437aaf36402928359c464f
Leonard Lan <real.leonard.lan@gmail.com>
2023-03-13 16:51:43 -0700

    Cleanup mutagen_tools and add ability to edit "year"

--- python/av_tools/mutagen_tools.py
+++ python/av_tools/mutagen_tools.py
@@ -3,49 +3,73 @@
 
 import mutagen
 
-
-KEY_TRACK = 'TRCK'
-KEY_TITLE = 'TIT2'
-KEY_ARTIST = 'TPE1'
-KEY_ALBUM = 'TALB'
+from mutagen.easyid3 import EasyID3
 
 
-def read_metadata(file_path):
-    '''
-    Read the track number, title, artist, and album from an audio or video file.
+KEY_TRACKNUMBER = 'tracknumber'
+KEY_TITLE = 'title'
+KEY_ARTIST = 'artist'
+KEY_ALBUM = 'album'
+KEY_ALBUMARTIST = 'albumartist'
+KEY_DATE = 'date'
+KEY_GENRE = 'genre'
+KEY_YEAR = 'year'
+
+FRAME_ID_YEAR = 'TDRC'
+
+DEFAULT_READ_METADATA_KEYS = [KEY_ARTIST, KEY_ALBUM, KEY_TITLE, KEY_TRACKNUMBER]
+KNOWN_KEYS = [
+    KEY_TRACKNUMBER,
+    KEY_TITLE,
+    KEY_ARTIST,
+    KEY_ALBUM,
+    KEY_ALBUMARTIST,
+    KEY_DATE,
+    KEY_GENRE,
+    KEY_YEAR,
+]
+
+
+def read_metadata(file_path, keys=None):
+    '''Get metadata values from an audio or video file, given list of keys to query.
 
     Args:
-    file_path (str): The path of the file to read the metadata from.
+        file_path (str): The path of the file to read the metadata from.
+        keys ([str]): List of keys to query. Will return values in same order. Defaults to
+            DEFAULT_READ_METADATA_KEYS.
 
     Returns:
-    tuple: A tuple of four values: the track number, title, artist, and album. Any of these values
-    may be None if the metadata is not present in the file.
+        list: A list of values in same order as given list.
     '''
-    audio = mutagen.File(file_path)
-    track_num = audio.get(KEY_TRACK, [None])[0]
-    title = audio.get(KEY_TITLE, [None])[0]
-    artist = audio.get(KEY_ARTIST, [None])[0]
-    album = audio.get(KEY_ALBUM, [None])[0]
-    return track_num, title, artist, album
+    keys = keys or DEFAULT_READ_METADATA_KEYS
+    audio = mutagen.File(file_path, easy=True)
+
+    # Register year if requested, otherwise it won't work.
+    if KEY_YEAR in keys:
+        _register_year()
+
+    return [audio.get(key, [None])[0] for key in keys]
 
 
-def write_metadata(file_path, track_num, title, artist, album):
-    '''
-    Write the track number, title, artist, and album to an audio or video file.
+def write_metadata(file_path, **kwargs):
+    '''Write metadata of an audio or video file.
 
     Args:
-    file_path (str): The path of the file to write the metadata to.
-    track_num (int or str): The track number.
-    title (str): The title.
-    artist (str): The artist.
-    album (str): The album.
+        file_path (str): The path of the file to write the metadata to.
+    Kwargs:
+        Key to value of metadata to write. For example: {'artist': 'John', 'album': 'The Album'}.
+        See KNOWN_KEYS for keys.
 
     Returns:
-    None
+        None
     '''
-    audio = mutagen.File(file_path)
-    audio[KEY_TRACK] = str(track_num)
-    audio[KEY_TITLE] = title
-    audio[KEY_ARTIST] = artist
-    audio[KEY_ALBUM] = album
+    audio = mutagen.File(file_path, easy=True)
+    for key, val in kwargs.items():
+        if key == KEY_YEAR:
+            _register_year()
+        audio[key] = val
     audio.save()
+
+
+def _register_year():
+    EasyID3.RegisterTextKey(KEY_YEAR, FRAME_ID_YEAR)
