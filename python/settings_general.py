7e93422ce27d254e7acbe45a82ec8b870d24b614
Leonard Lan <real.leonard.lan@gmail.com>
2023-03-13 16:50:03 -0700

    Add settings_general.py for general settings

---
+++ python/settings_general.py
@@ -0,0 +1,5 @@
+'''General variables and settings.'''
+
+
+# Nine characters that can't be in a file or folder name in Windows.
+ILLEGAL_PATH_CHARACTERS = {'\\', '/', ':', '*', '?', '"', '<', '>', '|'}
