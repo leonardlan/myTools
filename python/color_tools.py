ae3d7fdf54d6a65417b978b2ec9854359407e8e3
Leonard Lan <real.leonard.lan@gmail.com>
2023-03-15 23:10:02 -0700

    get_colored_diff(): Use different format if both strings are too different.

--- python/color_tools.py
+++ python/color_tools.py
@@ -6,7 +6,8 @@ import colors
 
 def get_colored_diff(s1, s2, s1_color_func=colors.bright_red, s2_color_func=colors.green):
     '''Given two strings s1 and s2, returns new string with any differing characters between the two
-    strings highlighted in red and green. Useful for showing capitalization changes on a string.
+    strings highlighted in red (s1) and green (s2). Useful for showing capitalization changes on a
+    string. If both strings are not similar at all, print in "red(s1)" -> "green(s2)".
 
     Args:
         s1 (str): First string.
@@ -17,13 +18,19 @@ def get_colored_diff(s1, s2, s1_color_func=colors.bright_red, s2_color_func=colo
     '''
     # Compare by iterating over shorter string.
     res = ''
+    diff_count = 0
     min_len = min(len(s1), len(s2))
     for i in range(min_len):
         if s1[i] != s2[i]:
             res += s1_color_func(s1[i])  # Print red character from s1.
             res += s2_color_func(s2[i])  # Print green character from s2.
+            diff_count += 1
         else:
             res += s1[i]  # Print regular character if no difference.
+
+    # If both strings are too different (over 50%), print in different format.
+    if float(diff_count) / min_len > 0.5:
+        return '"{}" -> "{}"'.format(s1_color_func(s1), s2_color_func(s2))
 
     # Add on an extra missing characters from s1 or s2.
     if min_len < len(s1):
