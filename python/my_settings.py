425947ecab7522fb26197984abe5659a87c64f2d
Leonard Lan <real.leonard.lan@gmail.com>
2023-03-23 20:22:41 -0700

    Add MY_TEMP_DIR setting

--- python/my_settings.py
+++ python/my_settings.py
@@ -2,11 +2,15 @@
 
 import os
 import sys
+import tempfile
+
+tempfile.gettempdir()
 
 
 HOME = os.environ['HOME'] if sys.platform.startswith('linux') else os.environ['USERPROFILE']
 MAX_LINES = 50
 MYTOOLS = os.path.join(HOME, 'myTools')
+MY_TEMP_DIR = os.path.join(tempfile.gettempdir(), 'myTools')
 
 # Datetime string formats.
 # Ex: '2022-09-12 17:28:33'.
