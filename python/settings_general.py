8c1e930017d3351a2aa4b0ba8a0207f7192e6fee
Leonard Lan <real.leonard.lan@gmail.com>
2023-04-22 16:51:50 -0700

    Add +ILLEGAL_PATH_CHARACTERS_STR

--- python/settings_general.py
+++ python/settings_general.py
@@ -3,3 +3,4 @@
 
 # Nine characters that can't be in a file or folder name in Windows.
 ILLEGAL_PATH_CHARACTERS = {'\\', '/', ':', '*', '?', '"', '<', '>', '|'}
+ILLEGAL_PATH_CHARACTERS_STR = ''.join(ILLEGAL_PATH_CHARACTERS)  # As string.
