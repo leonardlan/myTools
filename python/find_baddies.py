9aa0e41b2e1baa11dfa2ca7117d48b064e6e53cc
Leonard Lan <real.leonard.lan@gmail.com>
2023-12-16 15:54:08 -0800

    Updated find_baddies() to include index of found bad item. Also moved around docstrings.

--- python/find_baddies.py
+++ python/find_baddies.py
@@ -1,4 +1,12 @@
-'''Contains find_baddies().
+'''When you pass a list of items to a function but it fails without specifying which item, use this
+divide-and-conquer algorithm to find all the bad item(s), given that the function call is
+func(items, **func_kwargs).
+
+Space complexity: O(n).
+Time complexity: O(n * log(n)).
+* n is number of items.
+
+If you iterate through items, that would cost O(n) time complexity.
 
 Example:
 >>> items = [1, 2, 3, 4, 5, 'fail1', 6, 7, 8, 9, 10, 'fail2']
@@ -24,16 +32,7 @@ Checking second half...
 
 
 def find_baddies(func, items, exception=Exception, **func_kwargs):
-    '''Uses divide-and-conquer algorithm to find all bad items in given list of items, when being
-    passed to a black-box func(items, **func_kwargs).
-
-    Space complexity: O(n).
-    Time complexity: O(n * log(n)).
-    * n is number of items.
-
-    Use this when you're running a function call on a list of items, but it's raising an exception
-    without telling you which one(s) failed. You could iterate through items, but that would cost
-    O(n) time complexity.
+    '''See module docstring.
 
     Args:
         func (func): Function that's failing.
@@ -49,7 +48,7 @@ def find_baddies(func, items, exception=Exception, **func_kwargs):
     return _find_baddies(func, items, exception, 0, **func_kwargs)
 
 
-def _find_baddies(func, items, exception, level, **func_kwargs):
+def _find_baddies(func, items, exception, level, index_offset=0, **func_kwargs):
     '''Recursive helper for find_baddies().'''
     indent = '\t' * level
     print('{}Items: {}'.format(indent, len(items)))
@@ -59,31 +58,31 @@ def _find_baddies(func, items, exception, level, **func_kwargs):
 
     # Check first half.
     first_half = items[0:halfway]
-    res = _check_half(func, first_half, 'first', exception, level, indent, **func_kwargs)
+    res = _check_half(func, first_half, 'first', exception, level, index_offset, indent, **func_kwargs)
     if res:
         baddies.extend(res)
 
     # Check second half.
     second_half = items[halfway:]
-    res = _check_half(func, second_half, 'second', exception, level, indent, **func_kwargs)
+    res = _check_half(func, second_half, 'second', exception, level, index_offset + halfway, indent, **func_kwargs)
     if res:
         baddies.extend(res)
 
     return baddies
 
 
-def _check_half(func, half, first_or_second, exception, level, indent, **func_kwargs):
+def _check_half(func, half, first_or_second, exception, level, index_offset, indent, **func_kwargs):
     '''Run func on this half of arguments.'''
     try:
         func(half, **func_kwargs)
     except exception:
         if len(half) == 1:
             # Found a baddie.
-            print('{}Found {}'.format(indent, half[0]))
+            print('{}Found [{}]: {}'.format(indent, index_offset, half[0]))
             return half
         else:
             print('{}Checking {} half...'.format(indent, first_or_second))
-            res = _find_baddies(func, half, exception, level + 1)
+            res = _find_baddies(func, half, exception, level + 1, index_offset, **func_kwargs)
             return res
 
 
