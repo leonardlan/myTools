9222a7d501e6f1a9fa2ec9b2a21b237324db66e1
Leonard Lan <real.leonard.lan@gmail.com>
2024-09-06 23:02:29 -0700

    Add ls doskey cause Linux muscle memory

--- my_settings/windows/my_cmd_startup.bat
+++ my_settings/windows/my_cmd_startup.bat
@@ -29,6 +29,7 @@ set WORKSPACE_PATH=%DEV%\workspace
 set SUBLIME_PATH="C:\Program Files\Sublime Text 3\sublime_text.exe"
 
 :: My aliases/doskeys
+doskey ls=dir
 doskey h=doskey /history
 doskey py=python $*
 doskey p=python $*
