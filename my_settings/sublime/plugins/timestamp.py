a1e8a5010e058d80ce3fed0aff1836bfa760488b
Leonard Lan <real.leonard.lan@gmail.com>
2024-09-13 12:30:19 -0700

    Add docstring and switching indent from 2 to 4 spaces

--- my_settings/sublime/plugins/timestamp.py
+++ my_settings/sublime/plugins/timestamp.py
@@ -1,10 +1,23 @@
-""" Inserts timestamp at cursor location(s) """
+"""Insert current timestamp at cursor location(s).
+
+Example: [2024-09-13 11:53:59 AM]
+
+Install:
+1. Copy this file to "Sublime Text 3/Packages/User" folder.
+2. Setup a key binding under Preferences > Key Bindings. I use Ctrl+T, then S for "TimeStamp" here:
+{ "keys": ["ctrl+t", "s"], "command": "timestamp"}
+"""
+
+
 import datetime
 import sublime, sublime_plugin
 
 
+DATETIME_STR = "%Y-%m-%d %I:%M:%S %p"
+
+
 class TimestampCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    timestamp = "[%s]" % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    for sel in self.view.sel():
-        self.view.insert(edit, sel.begin(), timestamp)
+    def run(self, edit):
+        timestamp = "[%s]" % (datetime.datetime.now().strftime(DATETIME_STR))
+        for sel in self.view.sel():
+                self.view.insert(edit, sel.begin(), timestamp)
