1f462e442adb5f4e46e285dbc8dd96e34329f301
Leonard Lan <real.leonard.lan@gmail.com>
2026-01-02 01:18:07 -0800

    Fix bug with handling reloading in Python 3.14

--- python/python_compatibility.py
+++ python/python_compatibility.py
@@ -3,22 +3,27 @@
 import sys
 
 
-MAJOR_MINOR_FLOAT = float('{}.{}'.format(sys.version_info.major, sys.version_info.minor))
+PYTHON_VERSION_MAJOR = sys.version_info.major
+PYTHON_VERSION_MINOR = sys.version_info.minor
 
 
 def is_in_python_2():
-    return int(MAJOR_MINOR_FLOAT) == 2
+    return PYTHON_VERSION_MAJOR == 2
 
 
 def is_in_python_3():
-    return int(MAJOR_MINOR_FLOAT) == 3
+    return PYTHON_VERSION_MAJOR == 3
 
 
 def reload(module):
     '''Reload module backwards compatible for all Python versions. Why, Python, why?'''
-    if MAJOR_MINOR_FLOAT <= 2:
+    if is_in_python_2():
         reload(module)
-    if MAJOR_MINOR_FLOAT <= 3.3:
+        return
+
+    # Assume Python 3 here.
+    if PYTHON_VERSION_MINOR <= 3:
+        # Python 3, up to 3.3, use this:
         import imp
         imp.reload(module)
     else:
@@ -37,7 +42,7 @@ def is_string(data):
 
 def exec_file(file_path):
     '''Replace execfile() in Python 2.'''
-    if MAJOR_MINOR_FLOAT <= 2:
+    if is_in_python_2():
         execfile(file_path)
     else:
         exec(open(file_path).read())
